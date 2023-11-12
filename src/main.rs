48dd5612857187add32370f17b5e19402fd31365
Ellen Marie Dash <me@duckie.co>
2023-11-11 23:33:02 -0500

    it atually parses properly now.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use pest_derive::Parser;
 #[grammar = "main.pest"]
 struct SystemParser;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum FolderMode {
     Append,
     Speak,
@@ -24,6 +24,8 @@ struct System<'a> {
     name: &'a str,
     description: &'a str,
     default: &'a str,
+    rows: u8,
+    cols: u8,
     folders: HashMap<&'a str, Folder<'a>>,
 }
 
@@ -63,7 +65,11 @@ fn parse(system: &str) -> Result<System, &'static str> {
     let description = metadata.get("description").ok_or("Expected 'description' to be specified.")?;
     let default = metadata.get("default").ok_or("Expected 'default' to be specified.")?;
 
-    Ok(System { name, description, default, folders })
+    println!("{:?}", metadata);
+    let rows = metadata.get("rows").ok_or("Expected 'rows' to be specified.")?.parse().unwrap();
+    let cols = metadata.get("cols").ok_or("Expected 'cols' to be specified.")?.parse().unwrap();
+
+    Ok(System { name, description, default, rows, cols, folders })
 }
 
 fn main() {
@@ -88,19 +94,35 @@ folder "Example Folder" (append)
 #[test]
 fn test_parser() {
     let system = r#"
-        #sgs
-
-        name = "Example system."
+        name = "Example System"
         description = "This is an example system."
+        default = "Example Folder"
 
         rows = 2
         cols = 3
 
-        :Example Folder (append)
-                "1"     "2"     "3"
-                "4"     "5"     "6"
-        .
+        folder "Example Folder" (append)
+                "0"     "1"     "2"
+                "3"     "4"     "5"
+        ;
     "#;
 
-    println!("{}",system);
+    let system = parse(system).unwrap();
+
+    assert_eq!("Example System", system.name);
+    assert_eq!("This is an example system.", system.description);
+    assert_eq!("Example Folder", system.default);
+
+    assert_eq!(2, system.rows);
+    assert_eq!(3, system.cols);
+
+    let folder = &system.folders["Example Folder"];
+    assert_eq!(FolderMode::Append, folder.mode);
+    assert_eq!(6, folder.buttons.len());
+    assert_eq!("0", folder.buttons[0]);
+    assert_eq!("1", folder.buttons[1]);
+    assert_eq!("2", folder.buttons[2]);
+    assert_eq!("3", folder.buttons[3]);
+    assert_eq!("4", folder.buttons[4]);
+    assert_eq!("5", folder.buttons[5]);
 }
